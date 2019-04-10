59bc23f960b1910b18941be27e975a228af101c8
gyo <gyo@gyo-mbp-home.local>
2019-04-11 04:51:05 +0900

    add alias.fish

---
+++ alias.fish
@@ -0,0 +1,71 @@
+# base command
+alias ls 'ls -GwF'
+alias ll 'ls -l'
+alias la 'll -a'
+
+# git
+alias g 'git'
+alias ga "g add"
+alias gbr 'g branch'
+alias gc 'g commit -v'
+alias gca 'gc --amend'
+alias gp 'g push'
+alias gd 'g diff --word-diff'
+alias gdc 'gd --cached'
+alias gs 'g st'
+alias gl 'g log --oneline --graph --decorate'
+alias gf 'g flow'
+alias gfs 'gf feature start'
+alias gff 'gf feature finish'
+alias gp 'g push'
+alias gpo 'gp origin'
+alias gpom 'gpo master'
+alias gphm 'g push heroku master'
+
+alias gpl 'g pull'
+alias gplo 'gpl origin'
+alias gplom 'gplo master'
+
+# tig
+alias t 'tig'
+alias ts 't status'
+
+# editor
+alias e 'atom'
+
+alias o 'open'
+
+# ruby / rails
+alias b bundle
+alias bi 'bundle install'
+alias be 'bundle exec'
+alias r rails
+alias ra rake
+
+# tmux
+alias tmux 'direnv exec / tmux'
+alias tm 'tmux'
+alias tma 'tmux attach -t'
+alias tml 'tmux list-window'
+alias tmls 'tmux list-sessions'
+alias tmks 'tmux kill-session -t'
+
+# heroku
+alias hk 'heroku'
+
+# github
+alias hb 'hub browse'
+alias hpr 'hub pull-request --browse'
+alias pr 'pivotal_tracker_pr create'
+
+alias mc 'm'
+alias m 'tmuxinator'
+
+# docker
+alias dk 'docker'
+alias dm 'docker-machine'
+alias dc 'docker-compose'
+
+# less
+alias lr 'less -R'
+
