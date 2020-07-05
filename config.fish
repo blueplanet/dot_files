d2f494b00b639747d8090961e9e08455294498a3
blueplanet <gyo@re-work.io>
2020-07-05 16:13:03 +0900

    save

--- config.fish
+++ config.fish
@@ -2,4 +2,7 @@ status --is-interactive; and source (anyenv init -|psub)
 
 set -x EDITOR code
 
+set -x KUBECONFIG $HOME/Dropbox/dev/okteto-kube.config:'(KUBECONFIG:-$HOME/.kube/config)'
+
 starship init fish | source
+
