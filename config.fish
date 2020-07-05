4918f8f56089190b346c29e7635db5973c6589be
blueplanet <gyo@re-work.io>
2020-07-05 17:11:24 +0900

    fix anyenv

--- config.fish
+++
@@ -1,8 +0,0 @@
-status --is-interactive; and source (anyenv init -|psub)
-
-set -x EDITOR code
-
-set -x KUBECONFIG $HOME/Dropbox/dev/okteto-kube.config:'(KUBECONFIG:-$HOME/.kube/config)'
-
-starship init fish | source
-
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,5 @@
+status --is-interactive; and source (anyenv init -|psub)
+
 set -x EDITOR code
 
 set -x KUBECONFIG $HOME/Dropbox/dev/okteto-kube.config:'(KUBECONFIG:-$HOME/.kube/config)'
